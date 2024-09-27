c621ae692ce63d5cfa1276a4ef99c01e56c4ea22
khadir <tolibxonovm@gmail.com>
2024-09-27 20:45:49 +0500

    loops

---
+++ app.js
@@ -0,0 +1,138 @@
+// const ism = prompt("ismni kiriting ");
+
+// const mehmonlar = [
+//   "Ahror",
+//   "Sarvarjon",
+//   "Dadaxon",
+//   "Bahrom",
+//   "MuhammadNurullox ",
+//   "Behzod ",
+//   "Davlaltjon ",
+//   "odil",
+//   "hoshim",
+//   "yahyobek",
+// ];
+// console.log(mehmonlar[Math.floor(Math.random() * (mehmonlar.length - 1))]);
+// console.log(mehmonlar[Math.floor(Math.random() * mehmonlar.length)]);
+// if (mehmonlar.includes(ism)) {
+//   console.log("true");
+// } else {
+//   console.log("fasle");
+// }
+
+// const a = prompt("sonni kiriting ");
+// let counter = [];
+// for (let i = 1; i <= a; i++) {
+//   if (i % 2 == 1) {
+//     counter.push(i);
+//     console.log(i);
+//   }
+// }
+// console.log(counter);
+// console.log(counter.length);
+
+/* 1-misol*/
+
+// const n = prompt(" n sonini kiriting ");
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+/* 2-misol*/
+
+// const n = prompt("n sonini kiriting");
+
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+/* 3-misol*/
+
+// const n = prompt("n sonini kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log("yigindisi = " + sum);
+
+/* 4-misol*/
+
+// const n = prompt("n sonini kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//     console.log(i);
+//   }
+// }
+// console.log(" juft sonlarni yigindisi = " + sum);
+
+/* 5-misol*/
+
+// const n = prompt("n sonini kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     sum += i;
+//     console.log(i);
+//   }
+// }
+// console.log(" toq sonlarni yigindisi = " + sum);
+
+/* 6-misol*/
+
+// const n = prompt("n sonini kiriting");
+
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     console.log(i);
+//   }
+// }
+// console.log(n + " sonning qoldiqsiz bolinuvchiilari ");
+
+/* 7-misol*/
+
+// const n = prompt("n sonini kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     console.log(i);
+//     sum += i;
+//   }
+// }
+// console.log(n + " sonning qoldiqsiz bolinuvchiilari ");
+// console.log(sum + " bolinivchilkarni yogindisi  ");
+
+/* 8-misol*/
+
+// const n = prompt("n sonini kiriting");
+// for (let i = 2; i <= n; i++) {
+//   if (n % i === 0) {
+//     console.log("bu tub son emas ");
+//   } else {
+//     ("bu tub son !");
+//   }
+// }
+
+// function isPrime(num) {
+//   if (num <= 1) return false;
+
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// let n = prompt("n sonni kiriting");
+// for (let i = 1; i <= n; i++) {
+//   if (isPrime(i)) {
+//     console.log(i + " tub son.");
+//   } else {
+//     console.log(i + " tub son emas.");
+//   }
+// }
+
+/* 9-misol*/
