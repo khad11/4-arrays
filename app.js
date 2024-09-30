b86392b4f9a0a767ef033f808fd9ec160c036872
khadir <tolibxonovm@gmail.com>
2024-09-30 08:36:16 +0500

    add

--- app.js
+++ app.js
@@ -136,3 +136,119 @@
 // }
 
 /* 9-misol*/
+
+// let k = prompt(" k soninoi kiriting ");
+// let n = prompt(" n soninoi kiriting ");
+
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+/* 10-misol*/
+
+// let a = prompt(" a soninoi kiriting ");
+// let b = prompt(" b soninoi kiriting ");
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+/* 11-misol*/
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting b adan katta bosin ");
+
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+// }
+
+// console.log(`soni ${b - a - 1}`);
+
+/* 12-misol*/
+
+// let a = prompt("1kg kanfet narxini kiriting");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i}kg  ${i * a} so'm`);
+// }
+
+/* 13-misol*/
+
+// let a = prompt("1kg konfet narxini kiriting");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i * 100}gr ${(a * i) / 10} so'm`);
+// }
+
+/* 14-misol*/
+
+// let a = prompt("1kg konfet narxini kiriting");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i / 10 + 1}kg = ${a * (i / 10 + 1)} so'm`);
+// }
+
+/* 15-misol*/
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b) a soni b sonidan kichik bolsisn");
+// let m = b - a + 1; // // m bu a dan b gacha bo'lgan raqamlar soni
+
+// console.log(((Number(a) + Number(b)) / 2) * m);
+
+/* 16-misol*/
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)  a soni b sonidan kichik bolsisn");
+// let n = 1;
+
+// for (let i = a; i <= b; i++) {
+//   n *= i;
+// }
+
+// console.log(`ko'paytmasi ${n} ga teng`);
+
+/* 17-misol*/
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b) a soni b sonidan kichik bolsisn");
+// let n = 1;
+
+// for (let i = a; i <= b; i++) {
+//   n += i * i;
+// }
+
+// console.log(`kvadratlarining yig'indisi ${n - 1} ga teng`);
+
+/* 18-misol*/
+
+// let n = prompt("n sonini kiriting (n>0 n soni 0 lad katta bolsin)");
+// let s = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   s += 1 / i;
+// }
+
+// console.log(`S = ${s}`);
+
+/* 19-misol*/
+
+// let n = prompt("n sonini kiriting (n>0 n soni 0 lad katta bolsin)");
+// let s = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   s += i * i;
+// }
+
+// console.log(`S = ${s - 2}`);
+
+/* 20-misol*/
+
+// let n = prompt("n sonini kiriting (n>0 n soni 0 dan katta bolsin )");
+// let s = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   s *= 1 + i / 10;
+// }
+
+// console.log(`S = ${s}`);
